7de49e1170c0dd2256fdc261a55e8f4f48599fa4
Yeonsang Shin <samshin3910@snu.ac.kr>
2021-04-15 15:17:37 +0900

    [DOCS] Question1.py

--- pandas/Question1.py
+++ pandas/Question1.py
@@ -8,18 +8,18 @@ import pandas as pd
 my_2darray = np.array([[1, 2, 3], [4, 5, 6]])
 print(pd.DataFrame(my_2darray))
 
-print("------------------")
 # Take a dictionary as input to your DataFrame
+print("------------------")
 my_dict = {1: ['1', '3'], 2: ['1', '2'], 3: ['2', '4']}
 print(pd.DataFrame(my_dict))
 
-print("------------------")
 # Take a DataFrame as input to your DataFrame
+print("------------------")
 my_df = pd.DataFrame(data=[4, 5, 6, 7], index=range(10, 14), columns=['A'])
 print(pd.DataFrame(my_df))
 
-print("------------------")
 # Take a Series as input to your DataFrame
+print("------------------")
 my_series = pd.Series(
     {"United Kingdom": "London", "India": "New Delhi", "United States": "Washington", "Belgium": "Brussels"})
 print(pd.DataFrame(my_series))
