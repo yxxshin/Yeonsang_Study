fb7da9a74af0a101747edb0375917204c22a89f2
Yeonsang Shin <samshin3910@snu.ac.kr>
2021-04-15 15:09:41 +0900

    [DOCS] Question1.py

--- pandas/Question1.py
+++ pandas/Question1.py
@@ -5,26 +5,27 @@ import numpy as np
 import pandas as pd
 
 # Take a 2D array as input to your DataFrame
-my_2darray = np.array([ [1,2,3], [4,5,6] ])
+my_2darray = np.array([[1, 2, 3], [4, 5, 6]])
 print(pd.DataFrame(my_2darray))
 
 print("------------------")
 # Take a dictionary as input to your DataFrame
-my_dict = {1: ['1', '3'], 2: ['1', '2'], 3: ['2', '4'] }
+my_dict = {1: ['1', '3'], 2: ['1', '2'], 3: ['2', '4']}
 print(pd.DataFrame(my_dict))
 
 print("------------------")
 # Take a DataFrame as input to your DataFrame
-my_df = pd.DataFrame(data=[4,5,6,7], index=range(10,14), columns=['A'])
+my_df = pd.DataFrame(data=[4, 5, 6, 7], index=range(10, 14), columns=['A'])
 print(pd.DataFrame(my_df))
 
 print("------------------")
 # Take a Series as input to your DataFrame
-my_series = pd.Series({"United Kingdom":"London", "India":"New Delhi","United States":"Washington","Belgium":"Brussels"})
+my_series = pd.Series(
+    {"United Kingdom": "London", "India": "New Delhi", "United States": "Washington", "Belgium": "Brussels"})
 print(pd.DataFrame(my_series))
 
 print("------------------")
-df = pd.DataFrame(np.array([ [1,2,3], [4,5,6] ]))
+df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6]]))
 
 # Use the 'shape' property
 print(df.shape)
