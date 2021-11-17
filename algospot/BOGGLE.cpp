91b1cd941d96f057ce6f5dfd72cfa87f14bb980a
Yeon Sang Shin <samshin3910@snu.ac.kr>
2021-11-17 22:51:27 +0900

    [DOCS] BOGGLE.cpp

--- algospot/BOGGLE.cpp
+++ algospot/BOGGLE.cpp
@@ -1,38 +1,47 @@
 #include <stdio.h>
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
 int direction[8][2] = { {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0} };
 bool isExist;
+bool visited[6][6][11]; // visited[x][y][idx]: Is the case 'board[x][y] is the idx'th char of target' able? 
+char board[6][6];
+char target[11];
 
-void DFS(char board[][6], string target, int x, int y, int from) {
-    // If the target is found, change the flag
-    if(from == target.length()) {
-        isExist = true;
+void DP(int x, int y, int from) {
+    // If already visited, exit (Dynamic Programming)
+    if(visited[x][y][from]) {
+        return;
     }
     
-    // Else, test 8 directions from (x, y)
+    visited[x][y][from] = true;
+    // Check if the char matches
+    if(board[x][y] != target[from]) {
+        return;
+    }
+    
+    // If the word is found,
+    if(from == strlen(target)-1) {
+        isExist = true;
+        return;
+    }
+    
+    // Then, test 8 directions from (x, y)
     for(int i = 0; i < 8; i++) {
         int nextX = x + direction[i][0];
         int nextY = y + direction[i][1];
         if(nextX >= 0 && nextX < 5 && nextY >= 0 && nextY < 5) {
-            if(board[nextX][nextY] == target[from]) {
-                // Go to next level
-                DFS(board, target, nextX, nextY, from+1);
-            }
+            // Test the next level
+            DP(nextX, nextY, from+1);
         }
     }
 }
 
-void Solve(char board[][6], string target) {
-    isExist = false;
+void Solve() {
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
-            if(board[i][j] == target[0]) {
-                // First letter is same, start DFS
-                DFS(board, target, i, j, 1);
-            }
+            DP(i, j, 0);
         }
     }
     
@@ -46,18 +55,22 @@ int main()
     scanf("%d", &C);
     while(C--) {
         // Input Boggle board
-        char board[6][6];
         for(int i = 0; i < 5; i++) {
             scanf("%s", board[i]);
         }
         
         scanf("%d", &N);
         while(N--) {
-            // Using DFS for searching
-            string target;
-            cin >> target;
-            Solve(board, target);
+            memset(visited, 0, sizeof(visited));    // Initialize to all-zeros
+            isExist = false;
+            
+            scanf("%s", target);
+            printf("%s ", target);
+            
+            // Using Dynamic Programming
+            Solve();
         }
     }
 }
+
 
